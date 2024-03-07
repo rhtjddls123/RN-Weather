59a799a0068500d56fb9f4a3fd43d183e14494f0
rhtjddls123 <jjjk0605@naver.com>
2024-03-07 14:48:05 +0900

    Design: 첫 화면 만들기
    View를 통해 지역 이름 설정
    ScrollView를 통해 현재 화면 크기에 맞게 Day사이즈 조정, 좌우 스크롤,
    스크롤바 숨기기

--- App.tsx
+++ App.tsx
@@ -1,11 +1,40 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Text, View, ScrollView, Dimensions } from "react-native";
+import styled from "styled-components/native";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
   return (
-    <View className=" flex-1 items-center justify-center">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className=" flex-1 bg-red-500">
+      <View className=" flex-1 items-center justify-center">
+        <Text className=" font-medium text-7xl">Seoul</Text>
+      </View>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      >
+        <Day SCREEN_WIDTH={SCREEN_WIDTH}>
+          <Text className=" text-[178px] mt-[50]">27</Text>
+          <Text className=" text-[60px] -mt-[30]">Sunny</Text>
+        </Day>
+        <Day SCREEN_WIDTH={SCREEN_WIDTH}>
+          <Text className=" text-[178px] mt-[50]">27</Text>
+          <Text className=" text-[60px] -mt-[30]">Sunny</Text>
+        </Day>
+        <Day SCREEN_WIDTH={SCREEN_WIDTH}>
+          <Text className=" text-[178px] mt-[50]">27</Text>
+          <Text className=" text-[60px] -mt-[30]">Sunny</Text>
+        </Day>
+      </ScrollView>
     </View>
   );
 }
+
+type DayStyle = {
+  SCREEN_WIDTH: number;
+};
+const Day = styled.View<DayStyle>`
+  width: ${(props) => `${props.SCREEN_WIDTH}px`};
+  align-items: center;
+`;
