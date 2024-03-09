30d820260b82079f99e3828cee524d7494fff727
rhtjddls123 <jjjk0605@naver.com>
2024-03-09 15:12:13 +0900

    Feat: 사용자 위치정보 가져오기
    expo-location을 통해 사용자 위치정보를 받아와 현재 도시를 표시하도록
    변경

--- App.tsx
+++ App.tsx
@@ -1,13 +1,34 @@
+import { SetStateAction, useEffect, useState } from "react";
 import { Text, View, ScrollView, Dimensions } from "react-native";
 import styled from "styled-components/native";
+import * as Location from "expo-location";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [city, setCity] = useState<String | undefined>("Loading...");
+  const [ok, setOk] = useState(true);
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 3 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city?.toString());
+  };
+  useEffect(() => {
+    getWeather();
+  }, []);
   return (
     <View className=" flex-1 bg-red-500">
       <View className=" flex-1 items-center justify-center">
-        <Text className=" font-medium text-7xl">Seoul</Text>
+        <Text className=" font-medium text-7xl">{city?.toString()}</Text>
       </View>
       <ScrollView
         pagingEnabled
